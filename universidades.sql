89886680979910e2b689f16a2812055381fc462f
MRBONESMX <amir.zaratetinajero@gmail.com>
2026-08-25 22:27:04 -0700

    Add files via upload

---
+++ universidades.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.estudiantes (matricula,nombre,apellido,email,telefono,fecha_nacimiento,id_carrera,fecha_ingreso,estado) VALUES
+	 ('20260002','Maria','Lopez','maria@universidad.mx','6122222222','2004-08-15',7,'2026-08-20','ACTIVO'),
+	 ('20260003','Carlos','Ramírez','carlos@universidad.mx','6123333333','2005-01-20',8,'2026-08-20','ACTIVO'),
+	 ('20260004','Ana','Torres','ana@universidad.mx','6124444444','2004-11-03',9,'2026-08-20','ACTIVO'),
+	 ('20260001','Juan','Peréz','Juan@universidad.mx','6121111111','2005-06-10',7,'2026-08-20','ACTIVO');
